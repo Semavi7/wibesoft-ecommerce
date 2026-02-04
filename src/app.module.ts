f861cdabf47da0b0012ad682c3e86c6e7604e69d
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2026-02-04 13:13:53 +0300

    WEC-12: Add TypeORM migrations and disable synchronize for production

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,38 +1,23 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from './modules/products/entities/product.entity';
 import { ProductsModule } from './modules/products/products.module';
-import { User } from './modules/users/entities/user.entity';
 import { UsersModule } from './modules/users/users.module';
-import { Cart } from './modules/cart/entities/cart.entity';
-import { CartItem } from './modules/cart/entities/cart-item.entity';
 import { CartModule } from './modules/cart/cart.module';
-import { Order } from './modules/order/entities/order.entity';
-import { OrderItem } from './modules/order/entities/order-item.entity';
 import { OrdersModule } from './modules/order/order.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { typeOrmConfig } from './common/db/typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (ConfigService: ConfigService) => ({
-        type: "postgres",
-        host: ConfigService.get('DB_HOST'),
-        port: ConfigService.get('DB_PORT'),
-        username: ConfigService.get('DB_USERNAME'),
-        password: ConfigService.get('DB_PASSWORD'),
-        database: ConfigService.get('DB_NAME'),
-        entities: [Product, User, Cart, CartItem, Order, OrderItem],
-        synchronize: true
-      }),
-      inject: [ConfigService]
+    TypeOrmModule.forRoot({
+      ...typeOrmConfig,
+      autoLoadEntities: true,
     }),
     ProductsModule,
     UsersModule,
